8ca8b8caccfafa2ba1ba986539d673a0d66ebf3b
Rishov Chatterjee <garpchoo@gmail.com>
2018-03-21 04:34:19 -0700

    JS Challenge 1

---
+++ js/challenge1/data.js
@@ -0,0 +1,80 @@
+var mcDonalds = {
+  name: 'McDonalds',
+  branch: 'Rancho Cucamonga'
+  companyType: 'food',
+  dailySales: [
+    250,
+    482,
+    901,
+    549,
+    291,
+    387,
+    80
+  ]
+}
+
+var burgerKing = {
+  name: 'Burger King',
+  branch: 'Rancho Cucamonga',
+  companyType: 'food',
+  dailySales: [
+    502,
+    284,
+    109,
+    945,
+    912,
+    738,
+    248
+  ]
+}
+
+var wendys = {
+  name: 'Wendys',
+  branch: 'Rancho Cucamonga',
+  companyType: 'food',
+  dailySales: [
+    205,
+    428,
+    91,
+    495,
+    219,
+    378,
+    824
+  ]
+}
+
+var jackBox = {
+  name: 'Jack in the Box',
+  branch: 'Rancho Cucamonga',
+  companyType: 'food',
+  dailySales: [
+    300,
+    520,
+    102,
+    392,
+    190,
+    99,
+    972
+  ]
+}
+
+var kFC = {
+  name: 'KFC',
+  branch: 'Rancho Cucamonga',
+  companyType: 'food',
+  dailySales: [
+    792,
+    83,
+    91,
+    101,
+    822,
+    373,
+    891
+  ]
+}
+
+module.exports = mcDonalds
+module.exports = burgerKing
+module.exports = wendys
+module.exports = jackBox
+module.exports = kFC 
